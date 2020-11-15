b3b3b1d451340e15d8763518fad6a6fd8275ca63
Hannah Jenkins <hannahbrittany@hotmail.com>
2020-11-15 01:43:19 -0500

    created mutation gql client side

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,59 @@
+import gql from 'graphql-tag';
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const LOGIN_USER = gql`
+    mutation login($username: String!, $email: String!, $password: String!) {
+        login(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($authors: [String!], $description: String, $title: String!, $bookId: ID!, $link: String!) {
+        saveBook(authors: $authors, description: $description, title: $title, bookId: $bookId, link: $link) {
+            _id
+            username
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
