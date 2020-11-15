a9f29f62f906f76b30c5e280bc13233965734db4
Hannah Jenkins <hannahbrittany@hotmail.com>
2020-11-15 00:30:22 -0500

    fixed typedef error

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,7 +5,7 @@ const typeDefs = gql`
     }
     type Mutation {
         login(email: String!, password: String!): Auth
-        addUser(usernameL String!, email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
         saveBook (
             authors:[String]!,
             description: String!,
